c547255265f75fd68fee840fded87d25735485f4
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-02-01 00:19:59 +0530

    Port set_zero from JavaScript to Java
     Java class that implements the same 'set matrix zeroes' algorithm. Adds set_zero.java containing setmatrix(int[][]) and a main method that reads n, m and matrix input via Scanner, applies the zeroing logic, and prints the result. Also includes the compiled set_zero.class binary. Removed the JS driver/sample code and adapted marker/first-row/first-column handling to Java.

--- set_zero.java
+++ set_zero.java
@@ -1,59 +1,49 @@
-function setMatrixZeroes(mat) {
-    const n = mat.length, m = mat[0].length;
-    let c0 = 1;
-
-    // Traverse the array and mark 
-    // first cell of each row and column
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < m; j++) {
-            if (mat[i][j] === 0) {
-
-                // mark i-th row
-                mat[i][0] = 0;
-
-                // mark j-th column
-                if (j === 0)
-                    c0 = 0;
-                else
-                    mat[0][j] = 0;
+// Set matrix zero
+import java.util.*;
+class set_zero{
+    static int[][] setmatrix(int[][] arr){
+        int n=arr.length;
+        int m=arr[0].length;
+        int c=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(arr[i][j]==0){
+                    arr[i][0]=0;
+                    if(j==0) c=0;
+                    else arr[0][j]=0;
+                }
             }
         }
-    }
-
-    // Traverse and mark the matrix 
-    // from (1, 1) to (n - 1, m - 1)
-    for (let i = 1; i < n; i++) {
-        for (let j = 1; j < m; j++) {
-
-            // Check for col & row
-            if (mat[i][0] === 0 || mat[0][j] === 0) {
-                mat[i][j] = 0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(arr[i][0]==0|| arr[0][j]==0) arr[i][j]=0;
             }
         }
+        if(arr[0][0]==0){
+            for(int i=0;i<m;i++) arr[0][i]=0;
+        }
+        if(c==0){
+            for(int i=0;i<n;i++) arr[i][0]=0;
+        }
+        return arr;
     }
-
-    // Mark the first row
-    if (mat[0][0] === 0) {
-        for (let j = 0; j < m; j++)
-            mat[0][j] = 0;
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] arr=new int[n][m];  
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int[][] ans=setmatrix(arr);
+        System.out.println("Result: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-
-    // Mark the first column
-    if (c0 === 0) {
-        for (let i = 0; i < n; i++)
-            mat[i][0] = 0;
-    }
-}
-
-// Driver Code
-let mat = [
-    [0, 1, 2, 0],
-    [3, 4, 0, 2],
-    [1, 3, 1, 5]
-];
-
-setMatrixZeroes(mat);
-
-for (let row of mat) {
-    console.log(row.join(" "));
 }
