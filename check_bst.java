542a52713a081b34a42ed41bcc62e6fb793499e9
Unnati Kulshrestha <unkl.dolly@gmail.com>
2026-03-23 00:51:58 +0530

    Add BST validation and node classes
    Add check_bst.java implementing a simple binary search tree: a node class, insert method (BST insertion) and bst() method that validates the tree using recursive min/max bounds.

---
+++ check_bst.java
@@ -0,0 +1,41 @@
+//check is tree given is bst or not
+import java.util.*;
+class node{
+    int data;
+    node left,right;
+    node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}   
+class check_bst{
+    node root;
+    boolean bst(node root,int min,int max){
+        if(root==null) {
+            //System.out.println("bst");
+            return true;            
+        }
+        if(root.data<=min || root.data>=max) return false;
+        return bst(root.left,min,root.data) && bst(root.right,root.data,max);
+    }
+    public static void main(String args[]){
+        check_bst tree=new check_bst();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int val=sc.nextInt();
+            tree.root=tree.insert(tree.root,val);
+        }
+        if(tree.bst(tree.root,Integer.MIN_VALUE,Integer.MAX_VALUE)) System.out.println("BST");
+        else System.out.println("Not BST");
+
+
+    }
+    //bst order inserion
+    public node insert(node root,int data){
+        if(root==null) return new node(data);
+        if(data<root.data) root.left=insert(root.left,data);
+        else if(data>root.data) root.right=insert(root.right,data);
+        return root;
+    }
+}
